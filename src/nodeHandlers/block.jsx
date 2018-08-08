465a3ee4f2b20c9c3377841b1cb17d520f7b6cb4
Walery Jadłowski <valery@rebelmouse.com>
2018-08-08 15:02:41 +0200

    mimic stateless for block handler as it doesn't need state

--- src/nodeHandlers/block.jsx
+++ src/nodeHandlers/block.jsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 // TODO: Could be React.SFC but for the moment it doesn't support
 // Array return values supported in react 16 for typescript
 /// ref issue: https://github.com/DefinitelyTyped/DefinitelyTyped/pull/25349
-export class BlockHandler extends React.Component<{}, {}> {
+export class BlockHandler extends React.Component<{}, null> {
   render() {
     // tslint:disable-next-line
     console.log('some block logging added');
