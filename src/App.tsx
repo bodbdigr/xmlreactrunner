c27199d3a2d3983f2d5ccab3a5b3e2bc9749fe6d
Walery Jadłowski <valery@rebelmouse.com>
2018-08-08 17:08:16 +0200

    handle text nodes and plain html tags

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ const MenuButton:React.SFC = () => {
 
 const xml = `
   <block name="Some Page">
+    <h2>Sirui T 1205X</h2>
     <topbar format="simple">
       <menu_button />
     </topbar>
@@ -30,6 +31,15 @@ const xmlToComponentMap = {
 };
 
 const parser = new DOMParser();
+
+const validTags = `a abbr address area article aside audio b base bdi bdo big
+blockquote body br button canvas caption cite code col colgroup data datalist
+dd del details dfn dialog div dl dt em embed fieldset figcaption figure footer
+form h1 h2 h3 h4 h5 h6 head header hr html i iframe img input ins kbd keygen
+label legend li link main map mark menu menuitem meta meter nav noscript object
+ol optgroup option output p param picture pre progress q rp rt ruby s samp
+script section select small source span strong style sub summary sup table tbody
+td textarea tfoot th thead time title tr track u ul var video wbr`.split(' ');
 
 class App extends React.Component<{}, {}> {
   public render() {
@@ -46,10 +56,24 @@ class App extends React.Component<{}, {}> {
 
   private processXml(doc: Document | Element) {
     return Array.from(doc.children).map((node, idx) => {
-      const Component = xmlToComponentMap[node.nodeName];
+      const Component = validTags.indexOf(node.nodeName) !== -1 &&
+                          node.nodeName
+                          ||
+                          xmlToComponentMap[node.nodeName];
+
+      if (node.nodeType === 3) {
+        return node.nodeValue;
+      }
+
+      console.log('component is', Component, node.textContent) //tslint:disable-line
+
       return (
         <Component key={idx} {...this.nodeAttributesToDict(node)}>
-          {this.processXml(node)}
+          {
+            node.children.length ?
+            this.processXml(node) :
+            node.textContent
+          }
         </Component>
       );
     });
